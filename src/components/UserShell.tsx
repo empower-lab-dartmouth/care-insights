98dda331b203682db952e86775a9c82c9dcba1cd
Pape Sow Traoré <papisline2222@gmail.com>
2024-07-02 12:38:37 -0400

    quick info

--- src/components/UserShell.tsx
+++ src/components/UserShell.tsx
@@ -4,6 +4,7 @@ import {
   Burger,
   Divider,
   UnstyledButton,
+  Button,
   Text,
   Title,
 } from '@mantine/core';
@@ -21,6 +22,7 @@ import {
 import { primaryColor } from '../constants';
 import { useContext } from 'react';
 import { AuthContext } from '../state/context/auth-context';
+import { IconLogout, IconMenu2 } from '@tabler/icons-react';
 
 const MenuButton = ({
   children,
@@ -38,7 +40,7 @@ const MenuButton = ({
         className={`px-2 py-3 hover:bg-slate-100 rounded-md w-full flex items-center gap-2 text-sm`}
         style={{
           backgroundColor: pathname === path ? '#e7f5ff' : 'transparent',
-          color: pathname === path ? '#1971c2' : 'inherit',
+          color: pathname === path ? 'black' : 'white',
           fontWeight: pathname === path ? 'bold' : '500',
         }}
       >
@@ -60,7 +62,7 @@ const UserShell = ({ children }: { children: React.ReactNode }) => {
       }}
       navbar={{
         width: 220,
-        breakpoint: 'sm',
+        breakpoint: 'md',
         collapsed: { mobile: !opened },
       }}
       padding='md'
@@ -68,19 +70,27 @@ const UserShell = ({ children }: { children: React.ReactNode }) => {
       <AppShell.Header>
         <div className='flex justify-between items-center h-full px-3'>
           <div className='flex gap-2 items-center'>
-            <img src='logo-circle.svg' />
-            <h1 id='logo'>Care Insights</h1>
+            <UnstyledButton className='flex lg:hidden'>
+              <IconMenu2 size={24} />
+            </UnstyledButton>
+            <div className='hidden lg:flex gap-2 items-center'>
+              <img src='logo-circle.svg' />
+              <h1 id='logo'>Care Insights</h1>
+            </div>
           </div>
           <div className='flex items-center gap-2'>
             <Text>{currentUser!.email}</Text>
             <Avatar radius='xl' size='md' color='blue'>
               M
             </Avatar>
+            <UnstyledButton color='red' onClick={signOut}>
+              <IconLogout size={24} color='red' />
+            </UnstyledButton>
           </div>
         </div>
       </AppShell.Header>
 
-      <AppShell.Navbar p='sm' className='bg-[#f9f9f9]'>
+      <AppShell.Navbar p='sm' className='bg-primary'>
         <div className='flex flex-col justify-between h-full'>
           <div className='flex flex-col gap-2'>
             <MenuButton path='/info' icon={<Info size={18} />}>
@@ -96,20 +106,13 @@ const UserShell = ({ children }: { children: React.ReactNode }) => {
               Care Team
             </MenuButton>
           </div>
-          <div className='pb-3'>
-            <Divider className='pb-4' />
-            <UnstyledButton
-              className='pl-2 flex gap-2 text-sm font-semibold text-red-500 items-center'
-              onClick={() => signOut()}
-            >
-              {' '}
-              <LogOut size={18} /> Sign Out
-            </UnstyledButton>
-          </div>
+          <div className='pb-3'>{/* <Divider className='pb-4' /> */}</div>
         </div>
       </AppShell.Navbar>
 
-      <AppShell.Main className='mx-10'>{children}</AppShell.Main>
+      <AppShell.Main className=' bg-gray-50'>
+        <div className='px-10'>{children}</div>
+      </AppShell.Main>
     </AppShell>
   );
 };
--- src/screens/summaryInsights/QuickFactsBox.tsx
+++ src/screens/summaryInsights/QuickFactsBox.tsx
@@ -3,174 +3,240 @@ import Nav from '../nav/NavBar';
 import './summaryInsights.css';
 import QuestionAndAnswerPanel from './QuestionAndAnswerPanel/QuestionAndAnswerPanel';
 import CommonCRActions from '../nav/CommonCRActions/CommonCRActions';
-import { NO_CR_SELECTED, careRecipientsInfoState, pageContextState, queriesForCurrentCGState } from '../../state/recoil';
+import {
+  NO_CR_SELECTED,
+  careRecipientsInfoState,
+  pageContextState,
+  queriesForCurrentCGState,
+} from '../../state/recoil';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import CircularProgress from '@mui/material/CircularProgress';
 import UserShell from '../../components/UserShell';
-import { Image, Text, Grid, Group, Stack, Paper, Title, Center, Button, Chip, Pill, Textarea } from '@mantine/core';
+import {
+  Image,
+  Text,
+  Grid,
+  Group,
+  Stack,
+  Paper,
+  Title,
+  Center,
+  Button,
+  Chip,
+  Pill,
+  Textarea,
+  Card,
+  Divider,
+} from '@mantine/core';
 import WYSIWYGEditor from './WYSIWYGEditor/WYSIWYGEditor';
-import { MDXEditor, headingsPlugin, imagePlugin, linkDialogPlugin, listsPlugin } from '@mdxeditor/editor';
+import {
+  MDXEditor,
+  headingsPlugin,
+  imagePlugin,
+  linkDialogPlugin,
+  listsPlugin,
+} from '@mdxeditor/editor';
 import { AuthContext } from '../../state/context/auth-context';
 import { QueryRecord } from '../../state/queryingTypes';
-import { askQuery, modifyWithFeedback, respondToApprovalFeedback } from '../../state/querying';
+import {
+  askQuery,
+  modifyWithFeedback,
+  respondToApprovalFeedback,
+} from '../../state/querying';
 import { delayThenDo } from '../../state/sampleData';
-import { IconCheck, IconAlertTriangle, IconX, IconEdit } from '@tabler/icons-react';
+import {
+  IconCheck,
+  IconAlertTriangle,
+  IconX,
+  IconEdit,
+  IconInfoCircle,
+} from '@tabler/icons-react';
 import { PageState } from '../../state/types';
-
-
+import { Divide } from 'lucide-react';
 
 export const LOADING_STRING = 'Loading...';
 
 type QuickFactsBoxProps = {
-    type: 'avoid' | 'do' | 'symptom' | 'redirection'
-}
+  type: 'avoid' | 'do' | 'symptom' | 'redirection';
+};
 
 const title = ({ type }: QuickFactsBoxProps) => {
-    switch (type) {
-        case 'avoid':
-            return "Avoid";
-        case 'do':
-            return 'Do';
-        case 'redirection':
-            return 'Redirection';
-        case 'symptom':
-            return 'Symptoms';
-    }
+  switch (type) {
+    case 'avoid':
+      return 'Avoid';
+    case 'do':
+      return 'Do';
+    case 'redirection':
+      return 'Redirection';
+    case 'symptom':
+      return 'Symptoms';
+  }
 };
 
-const responseChip = (loading: boolean,
-    alreadyApproved: boolean) => {
-    if (loading) {
-        return (<></>);
-    }
-    if (!alreadyApproved) {
-        return (<Pill c={'red'}>AI generated</Pill>);
-    }
-    return <></>
+const responseChip = (loading: boolean, alreadyApproved: boolean) => {
+  if (loading) {
+    return <></>;
+  }
+  if (!alreadyApproved) {
+    return <Pill c={'red'}>AI generated</Pill>;
+  }
+  return <></>;
 };
 
-const QuickFactsBoxInner: React.FC<QuickFactsBoxProps> = (props) => {
-    const { type } = props;
-    // const { currentUser } = useContext(AuthContext);
-    const [pageContext, setPageContext] = useRecoilState(pageContextState);
-    const [loadingResponse, setLoadingResponse] = useState(false);
-    const [queries, setQueries] = useRecoilState(queriesForCurrentCGState);
-    const [editingDirectly, setEditingDirectly] = useState(false);
-    const queryRecordQuery = type === 'do' ? pageContext.doQuery :
-        type === 'avoid' ? pageContext.avoidQuery :
-            type === 'redirection' ? pageContext.redirectionQuery :
-                pageContext.symptomsQuery;
-    const queryRecord = queries[queryRecordQuery];
-    console.log(queries);
-    console.log(queryRecordQuery);
-    console.log("query record query");
-    const setQueryRecord = (q: QueryRecord) => {
-        setQueries({
-            ...queries,
-            [q.query]: q
-        });
-    }
-    const [editedResponse, setEditedResponse] = useState(queryRecord
-        .queryResponse);
-    const alreadyApproved = queryRecord.dateApproved !== undefined;
+const QuickFactsBoxInner: React.FC<QuickFactsBoxProps> = props => {
+  const { type } = props;
+  // const { currentUser } = useContext(AuthContext);
+  const [pageContext, setPageContext] = useRecoilState(pageContextState);
+  const [loadingResponse, setLoadingResponse] = useState(false);
+  const [queries, setQueries] = useRecoilState(queriesForCurrentCGState);
+  const [editingDirectly, setEditingDirectly] = useState(false);
+  const queryRecordQuery =
+    type === 'do'
+      ? pageContext.doQuery
+      : type === 'avoid'
+      ? pageContext.avoidQuery
+      : type === 'redirection'
+      ? pageContext.redirectionQuery
+      : pageContext.symptomsQuery;
+  const queryRecord = queries[queryRecordQuery];
+  console.log(queries);
+  console.log(queryRecordQuery);
+  console.log('query record query');
+  const setQueryRecord = (q: QueryRecord) => {
+    setQueries({
+      ...queries,
+      [q.query]: q,
+    });
+  };
+  const [editedResponse, setEditedResponse] = useState(
+    queryRecord.queryResponse
+  );
+  const alreadyApproved = queryRecord.dateApproved !== undefined;
 
-    const submitApprovalFeedback = async () => {
-        setLoadingResponse(true);
-        const approvedQuery: QueryRecord = {
-            ...queryRecord,
-            dateApproved: (new Date()).getTime(),
-            queryResponse: editedResponse,
-        };
-        await respondToApprovalFeedback(
-            approvedQuery);
-        setQueryRecord(approvedQuery);
-        setLoadingResponse(false);
+  const submitApprovalFeedback = async () => {
+    setLoadingResponse(true);
+    const approvedQuery: QueryRecord = {
+      ...queryRecord,
+      dateApproved: new Date().getTime(),
+      queryResponse: editedResponse,
     };
-    const approve = () => {
-        submitApprovalFeedback();
-        setEditingDirectly(false);
-    };
+    await respondToApprovalFeedback(approvedQuery);
+    setQueryRecord(approvedQuery);
+    setLoadingResponse(false);
+  };
+  const approve = () => {
+    submitApprovalFeedback();
+    setEditingDirectly(false);
+  };
 
-    return (
-        <Paper>
-            <Stack style={{ minHeight: 300 }}>
-                <Group justify='flex-end' h={"auto"}>
-                    {responseChip(loadingResponse, alreadyApproved)}
-                </Group>
-                <Center>
-                    <Title order={1} c={type == 'avoid' ? 'red' : 'dark'}>{title(props)}</Title>
-                </Center>
-                {!editingDirectly ? <Text
-                    style={{
-                        "white-space": "pre-wrap"
-                    }} fz="sm" lh="md">
-                    {editedResponse}
-                </Text> :
-                    <Textarea
-                        label="Make edits below"
-                        value={editedResponse}
-                        onChange={(event) => setEditedResponse(event.currentTarget.value)}
-                        autosize
-                        minRows={2}
-                    />
-                }
+  return (
+    <Card className='mt-[30px] border border-gray-100 ' shadow='xs' p={0}>
+      <Stack style={{ minHeight: 300 }}>
+        <Group justify='flex-end' h={'auto'}>
+          {responseChip(loadingResponse, alreadyApproved)}
+        </Group>
+        <div>
+          <div className='px-4 pb-2 flex justify-between'>
+            <div className='flex items-center gap-2'>
+              <IconInfoCircle size={26} />
+              <Title order={3} c={type == 'avoid' ? 'red' : 'dark'}>
+                {title(props)}
+              </Title>
+            </div>
+            <Stack align='flex-end' justify='flex-end'>
+              <Group justify='flex-end' h={'auto'}>
+                <Button
+                  variant='transparent'
+                  onClick={approve}
+                  disabled={alreadyApproved && !editingDirectly}
+                  leftSection={<IconCheck color='green' size={14} />}
+                >
+                  {alreadyApproved && !editingDirectly ? 'Endorsed' : 'Endorse'}
+                </Button>
+                {editingDirectly ? (
+                  <Button
+                    variant='transparent'
+                    onClick={() => {
+                      if (queries[queryRecord.query] !== undefined) {
+                        setEditedResponse(
+                          queries[queryRecord.query].queryResponse
+                        );
+                      }
+                      setEditingDirectly(false);
+                    }}
+                    leftSection={<IconX size={14} />}
+                  >
+                    Cancel{' '}
+                  </Button>
+                ) : (
+                  <></>
+                )}
+                <Button
+                  variant='transparent'
+                  disabled={editingDirectly}
+                  onClick={() => {
+                    setEditingDirectly(true);
+                    setEditedResponse(pageContext.insightsQuery.queryResponse);
+                  }}
+                  leftSection={<IconEdit size={14} />}
+                >
+                  Make changes
+                </Button>
+              </Group>
             </Stack>
-            <Stack
-                align="flex-end"
-                justify="flex-end"
+          </div>
+          <Divider />
+        </div>
+
+        <div className='px-6 pt-2 pb-8'>
+          {!editingDirectly ? (
+            <Text
+              style={{
+                'white-space': 'pre-wrap',
+              }}
+              fz='sm'
+              lh='md'
             >
-                <Group justify="flex-end" h={"auto"}>
-
-                    <Button variant="outline" onClick={approve}
-                        disabled={alreadyApproved && !editingDirectly}
-                        leftSection={<IconCheck color='green' size={14} />} >
-                        {alreadyApproved && !editingDirectly ? "Endorsed" : "Endorse"}
-                    </Button>
-                    {
-                        editingDirectly ?
-                            <Button variant="outline"
-                                onClick={() => {
-                                    if (queries[
-                                        queryRecord.query] !== undefined) {
-                                        setEditedResponse(queries[
-                                            queryRecord.query].queryResponse);
-                                    }
-                                    setEditingDirectly(false);
-                                }}
-                                leftSection={<IconX size={14} />}>
-                                Cancel </Button> : <></>
-                    }
-                    <Button variant="outline"
-                        disabled={editingDirectly}
-                        onClick={() => {
-                            setEditingDirectly(true);
-                            setEditedResponse(pageContext
-                                .insightsQuery
-                                .queryResponse);
-                        }}
-                        leftSection={<IconEdit size={14} />}>
-                        Make changes
-                    </Button>
-                </Group>
-            </Stack>
-        </Paper>
-    );
+              {editedResponse}
+            </Text>
+          ) : (
+            <Textarea
+              label='Make edits below'
+              value={editedResponse}
+              onChange={event => setEditedResponse(event.currentTarget.value)}
+              autosize
+              minRows={2}
+            />
+          )}
+        </div>
+      </Stack>
+    </Card>
+  );
 };
 
-const QuickFactsBox: React.FC<QuickFactsBoxProps> = (props) => {
-    const { type } = props;
-    // const { currentUser } = useContext(AuthContext);
-    const [pageContext, setPageContext] = useRecoilState(pageContextState);
-    const [loadingResponse, setLoadingResponse] = useState(false);
-    const [queries, setQueries] = useRecoilState(queriesForCurrentCGState);
-    const queryRecordQuery = type === 'do' ? pageContext.doQuery :
-        type === 'avoid' ? pageContext.avoidQuery :
-            type === 'redirection' ? pageContext.redirectionQuery :
-                pageContext.symptomsQuery;
-    return (
-        <>{queries[queryRecordQuery] !== undefined ? <QuickFactsBoxInner type={type} /> : <CircularProgress />
-        }
-        </>);
-}
+const QuickFactsBox: React.FC<QuickFactsBoxProps> = props => {
+  const { type } = props;
+  // const { currentUser } = useContext(AuthContext);
+  const [pageContext, setPageContext] = useRecoilState(pageContextState);
+  const [loadingResponse, setLoadingResponse] = useState(false);
+  const [queries, setQueries] = useRecoilState(queriesForCurrentCGState);
+  const queryRecordQuery =
+    type === 'do'
+      ? pageContext.doQuery
+      : type === 'avoid'
+      ? pageContext.avoidQuery
+      : type === 'redirection'
+      ? pageContext.redirectionQuery
+      : pageContext.symptomsQuery;
+  return (
+    <>
+      {queries[queryRecordQuery] !== undefined ? (
+        <QuickFactsBoxInner type={type} />
+      ) : (
+        <CircularProgress />
+      )}
+    </>
+  );
+};
 
 export default QuickFactsBox;
