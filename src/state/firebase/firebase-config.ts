8201a8996ff0f1f91586ca71091619f90df2f901
Dylan <dylanedwardmoore@gmail.com>
2024-07-25 17:54:52 -0400

    update firebase config

--- src/state/firebase/firebase-config.ts
+++ src/state/firebase/firebase-config.ts
@@ -4,16 +4,27 @@ import {getFirestore} from 'firebase/firestore';
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
+const myFirebaseConfigApiKey = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_CONFIG_API_KEY;
+const myFirebaseConfigAuthDomain = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_CONFIG_AUTH_DOMAIN
+const myFirebaseConfigProjectId = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_CONFIG_PROJECT_ID
+const myFirebaseConfigStorageBucket = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_CONFIG_STORAGE_BUCKET
+const myFirebaseConfigMessageSenderId = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_CONFIG_MESSAGING_SENDER_ID
+const myFirebaseConfigAppId = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_CONFIG_APP_ID
+const myFirebaseMeasurementId = import.meta.env.VITE_EMPOWER_LAB_FIREBASE_MEASUREMENT_ID
+
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyCdjr9Jn3tqbYKaRSL9cPlPqT0TsSsVEe8',
-  authDomain: 'storygraph2.firebaseapp.com',
-  projectId: 'storygraph2',
-  storageBucket: 'storygraph2.appspot.com',
-  messagingSenderId: '1025377557908',
-  appId: '1:1025377557908:web:7160126d56e02092874381',
+  apiKey: myFirebaseConfigApiKey,
+  authDomain: myFirebaseConfigAuthDomain,
+  projectId: myFirebaseConfigProjectId,
+  storageBucket: myFirebaseConfigStorageBucket,
+  messagingSenderId: myFirebaseConfigMessageSenderId,
+  appId: myFirebaseConfigAppId,
+  measurementId: myFirebaseMeasurementId
 };
 
 // Initialize Firebase
 export const app = initializeApp(firebaseConfig);
+console.log('APP CONNECTION WORKED', app);
 export const db = getFirestore(app);
+console.log('DB CONNECTION WORKED', db);
